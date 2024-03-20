ee9d0e9450910f71d648975985b4c7c41e68d244
gr_rocket <141749145+gr-rocket@users.noreply.github.com>
2024-03-20 17:21:30 -0600

    Add files via upload
    Archivo js

---
+++ mi_script.js
@@ -0,0 +1,10 @@
+let visitas = localStorage.getItem('Contador_visitas');
+
+if (!visitas) {
+	visitas=0;
+}
+
+visitas++;
+localStorage.setItem('Contador_visitas',visitas);
+
+document.getElementById('Contador').textContent=visitas;
